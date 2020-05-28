e2ba30c8b0c1870bef8335cdb4ed327f8903d0d9
Carl-Rabbit <479843162@qq.com>
2020-05-28 08:57:37 +0800

    web finished

--- my12306_web/src/main/java/com/dbpp/my12306/controller/MainController.java
+++ my12306_web/src/main/java/com/dbpp/my12306/controller/MainController.java
@@ -1,18 +1,18 @@
-package com.dbpp.my12306.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class MainController {
-
-	@RequestMapping(value = "/")
-	public String index() {
-		return "redirect:index.html";
-	}
-
-	@RequestMapping(value = "/api/help")
-	public String help() {
-		return "api_help";
-	}
-}
+//package com.dbpp.my12306.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class MainController {
+//
+//	@RequestMapping(value = "/")
+//	public String index() {
+//		return "redirect:index.html";
+//	}
+//
+//	@RequestMapping(value = "/api/help")
+//	public String help() {
+//		return "api_help";
+//	}
+//}
